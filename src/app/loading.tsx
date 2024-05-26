8b1c5eb89c230eca7220b683ac4c336a825f6ee6
ranielcsar <whyfck@gmail.com>
2024-05-26 18:34:08 -0300

    del: página de loading removida

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <main className="bg-light-green h-screen w-full flex flex-col text-gray-700">
-      <h2 className="text-xl md:text-2xl font-bold m-auto">Carregando...</h2>
-    </main>
-  )
-}
