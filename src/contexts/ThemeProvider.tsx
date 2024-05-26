e9173e3062fe560e12930d1d6e2264f89a412bcf
ranielcsar <whyfck@gmail.com>
2024-05-26 12:57:42 -0300

    feat: theme provider do material design

---
+++ src/contexts/ThemeProvider.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { ThemeProvider as Provider, createTheme } from "@mui/material"
+import { PropsWithChildren } from "react"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5d00bf",
+    },
+  },
+})
+
+export function ThemeProvider({ children }: PropsWithChildren<any>) {
+  return <Provider theme={theme}>{children}</Provider>
+}
